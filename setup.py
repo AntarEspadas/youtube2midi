3a3a5a6ebb80d3084d65e6567ddcfd6ed4172d67
Naratna <antar.espadas@hotmail.com>
2021-05-04 21:23:44 -0500

    Added license classifier

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
     keywords=['python', 'midi', 'youtube'],
     classifiers=[
         "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: Microsoft :: Windows",
     ]
 )
