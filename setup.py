0efeb981340fcccff12e0c6d891354d90d26be36
Naratna <antar.espadas@hotmail.com>
2021-05-05 00:30:09 -0500

    Bumped version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from os.path import dirname, join
 
-VERSION = '0.1.0'
+VERSION = '0.1.1'
 DESCRIPTION = "A Python module with a small cli, used to automatically download a Synesthesia piano tutorial from a YouTube URL and convert it to a MIDI file."
 
 try:
@@ -15,12 +15,12 @@ setup(
     name="youtube2midi",
     license="MIT",
     url="https://github.com/Naratna/youtube2midi",
-    download_url="https://github.com/Naratna/youtube2midi/archive/refs/tags/v0.1.0.tar.gz",
+    download_url="https://github.com/Naratna/youtube2midi/archive/refs/tags/v0.1.1.tar.gz",
     version=VERSION,
     author="Naratna",
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
-    long_description_content_type="markdown",
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
